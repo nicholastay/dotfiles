7e0616ccd9d0479fab0c93f14116aa19bf8a76f7
Nicholas Tay <nkt@outlook.kr>
2019-12-11 11:20:56 +1100

    Dynamically spawn a polybar for every monitor instead of hardcode

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait for processes to be killed
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
-polybar second &
+# Spawn a bar for every display
+for mon in $(polybar -m | awk -F: '{ print $1 }'); do
+	MONITOR=$mon polybar main &
+done
 
 echo "Polybar OK"
