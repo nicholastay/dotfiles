a0d4734cdff44200d79177e9f05c8b5f20c9ee5c
Nicholas Tay <nkt@outlook.kr>
2020-11-20 12:17:49 +1100

    Add back in i3...

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait for processes to be killed
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Left module for wm
+[ -z "$POLYBAR_MODULES_LEFT" ] && export POLYBAR_MODULES_LEFT="xwindow"
+export POLYBAR_MODULES_LEFT="$NARB_WM $POLYBAR_MODULES_LEFT"
+
 # Spawn a bar for every display
 for mon in $(polybar -m | awk -F: '{ print $1 }'); do
 	MONITOR=$mon polybar main &
