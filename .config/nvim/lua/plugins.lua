61ca42c23ae94d2c3ee9b6742e06716d9e191ef4
Nicholas Tay <nick@windblume.net>
2025-03-30 12:54:26 +1100

    Disable nvim cmp crap
    It is pretty broken and this is on the older packer setup. Probably best
    to reconcile with the dotlite setup...

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -20,6 +20,7 @@ return require("packer").startup(function(use)
 		config = function()
 			require("tokyonight").setup {
 				style = "night",
+				transparent = true,
 			}
 			vim.cmd [[colorscheme tokyonight]]
 		end
@@ -52,6 +53,112 @@ return require("packer").startup(function(use)
 
 	-- Comment with 'gc' etc
 	use("tomtom/tcomment_vim")
+
+	-- --------------
+
+	-- Programming specific
+
+	-- Treesitter: Highlighting and more
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup {
+				ensure_installed = { "c", "lua", "python" },
+				highlight = {
+					enable = true,
+				},
+			}
+		end,
+	})
+
+	-- Autopairs: Pair brackets
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup {}
+		end,
+	})
+
+	-- surround
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup {}
+		end,
+	})
+	
+	-- LSP support
+	-- use("neovim/nvim-lspconfig")
+
+	-- nvim-cmp: Completion
+	-- use({
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	requires = {
+	-- 		"L3MON4D3/LuaSnip",
+	-- 		"saadparwaiz1/cmp_luasnip", -- Using luasnip as engine
+	-- 		{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" }, -- Built-in LSP source
+	-- 		{ "hrsh7th/cmp-buffer", after = "nvim-cmp" }, -- Completion with words in buffer
+	-- 	},
+	-- 	config = function()
+	-- 		local cmp = require("cmp")
+	-- 		local luasnip = require("luasnip")
+	-- 		cmp.setup {
+	-- 			snippet = {
+	-- 				expand = function(args)
+	-- 					luasnip.lsp_expand(args.body)
+	-- 				end,
+	-- 			},
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 				["<C-Space>"] = cmp.mapping.complete(),
+	-- 				["<C-e>"] = cmp.mapping.abort(),
+	-- 				["<CR>"] = cmp.mapping.confirm({ select = true }),
+	-- 				['<C-n>'] = cmp.mapping(function(fallback)
+	-- 					if cmp.visible() then
+	-- 						cmp.select_next_item()
+	-- 					elseif luasnip.expand_or_jumpable() then
+	-- 						luasnip.expand_or_jump()
+	-- 					elseif has_words_before() then
+	-- 						cmp.complete()
+	-- 					else
+	-- 						fallback()
+	-- 					end
+	-- 				end, { 'i', 's' }),
+	-- 				['<C-p>'] = cmp.mapping(function(fallback)
+	-- 					if cmp.visible() then
+	-- 						cmp.select_prev_item()
+	-- 					elseif luasnip.jumpable(-1) then
+	-- 						luasnip.jump(-1)
+	-- 					else
+	-- 						fallback()
+	-- 					end
+	-- 				end, { 'i', 's' }),
+	-- 			}),
+	-- 			sources = {
+	-- 				{ name = "nvim_lsp" },
+	-- 				{ name = "luasnip" },
+	-- 				{ name = "buffer" },
+	-- 			},
+	-- 		}
+    --
+	-- 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+	-- 		capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    --
+	-- 		local lsp = require('lspconfig')
+	-- 		lsp.clangd.setup{
+	-- 			capabilities = capabilites,
+	-- 			filetypes = { "c", "cpp" },
+	-- 		}
+	-- 	end,
+	-- })
+
+	-- --------------
 
 	if packer_bootstrap then
 		require("packer").sync()
