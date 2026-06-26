5a21b8f1fb78c9e7df0aa7c7669c6f108927f659
Nicholas Tay <nick@windblume.net>
2026-06-26 11:59:36 +1000

    updated nvim stuffs

--- .config/nvim/lua/bloat.lua
+++ .config/nvim/lua/bloat.lua
@@ -135,13 +135,7 @@ local plugins = {
 			require("mason-lspconfig").setup()
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
+			require("mason-lspconfig").setup()
 
 			vim.api.nvim_create_autocmd('LspAttach', {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
